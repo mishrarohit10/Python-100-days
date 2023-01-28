6053de5831c00e07490177e2c9b12c64f715fb3f
Rohit Mishra <89183101+mishrarohit10@users.noreply.github.com>
2023-01-28 20:42:40 +0530

    Add files via upload

---
+++ day10.py
@@ -0,0 +1,6 @@
+a = input("enter your name ")
+print("MY name is",a)
+x = input("enter 1st num ")
+y = input("enter 2nd num ")
+print(x+y)
+print(int(x) + int(y))
---
+++ day9.py
@@ -0,0 +1,10 @@
+a = '1'
+a1 = 1
+b1 = 3
+b = '3'
+print(a+b)
+print(int(a)+int(b))  #explicit
+print(a1+b1)
+c =1.9
+d=8
+print(c+d) #implicit typecasting
