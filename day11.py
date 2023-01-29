c166002602ea426b266822ba4b2b091ba8411169
Rohit Mishra <89183101+mishrarohit10@users.noreply.github.com>
2023-01-29 23:36:42 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,27 @@
+name = "BRUCE"
+print("NAME" + name)
+appe = "WOW HE SAID \"  WOEORWEOR\" "
+print(appe)
+wow = '''feards 
+eawsfgaserg
+fgas
+gfas
+drgds
+rg
+drg
+darsg
+drag
+adrgdarg
+egsra
+egsra'''
+print(wow)
+# print(wow)
+# print(wow)
+print(name[0])
+print(name[3])
+# print()
+print("Lets use a for loop\n")
+for character in name:
+  print(character)
+for character in wow:
+  print(character)
