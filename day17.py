6dcbe83c4dc48fb96612211953f674aad666041c
Rohit Mishra <89183101+mishrarohit10@users.noreply.github.com>
2023-01-31 23:01:45 +0530

    Add files via upload

---
+++ day17.py
@@ -0,0 +1,19 @@
+# name = 'Rohit'
+# for i in name: 
+#   print(i)
+#   if(i=='R'):
+#     print("alright")
+
+# color = ["1", "Red", "Blue", "Yellow"]
+# for color in color:
+#   print(color)
+#   for i in color:
+#     print(i)
+
+# for i in range(5):
+#   print(i+1)
+# for k in range(1,20001)  :
+#   print(k)
+
+for j in range(1,34,7):
+  print(j)
