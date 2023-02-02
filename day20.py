206745b1dfa83451ae95deaffc90fd0b7721bd1a
Rohit Mishra <89183101+mishrarohit10@users.noreply.github.com>
2023-02-02 22:53:43 +0530

    Add files via upload

---
+++ day20.py
@@ -0,0 +1,21 @@
+# a = 9
+# b = 4
+# gmean = (a*b)/(a+b)
+# print(gmean)
+
+def CalculateGmean(a,b):
+  mean = (a*b)/(a+b)
+  print(mean)
+
+CalculateGmean(8,9)
+
+def greater(a,b):
+  if(a>b):
+    print(a)
+  else:
+    print(b)
+
+greater(23,56)
+
+def wow(a,b):
+  pass
