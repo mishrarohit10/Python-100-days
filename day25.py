4909a6496f160383c62fb10d88fd22ec152e468b
Rohit Mishra <89183101+mishrarohit10@users.noreply.github.com>
2023-02-04 22:08:15 +0530

    Add files via upload

---
+++ day25.py
@@ -0,0 +1,12 @@
+names = ("r", "re","re3")
+nameslist = list(names)
+nameslist.append("ererw")
+nameslist.pop(2)
+nameslist[2] = "Watbe"
+names = tuple(nameslist)
+print(names)
+
+fname = ("werrewerwrw")
+lname = ("eswrfersgrg")
+name = fname + lname
+print(name)
